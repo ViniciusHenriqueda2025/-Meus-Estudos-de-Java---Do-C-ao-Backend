d509e766928a34e713ee1f328861d351b8f68f6f
VINICIUS HENRIQUE DA COSTA <viniciushenriquedacosta8@gmail.com>
2026-01-22 14:41:49 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package quarto;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite dois numeros: ");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+
+		if (n2 < n1){
+	
+		int troca = n1;
+		n1 = n2;
+		n2 = troca;
+		}
+		
+		int soma = 0;
+		
+		for ( int i = n1+1; i < n2; i++ ) {
+			if ( i %2 != 0 ) {
+		soma = soma	+ i;	
+			}
+		}
+		
+		System.out.print("SOMA DOS IMPARES= " + soma);
+		
+		
+		
+		
+	}
+
+}
